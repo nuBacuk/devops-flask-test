5bac8c88ceff28b769acb40fee6eaaa0bef05072
Dmitry Frolov <dvfrolov@mts.ru>
2020-03-31 08:54:57 +0300

    run app

--- app.py
+++ app.py
@@ -15,3 +15,5 @@ def metrics():
     global counter
     result = "# TYPE hello_world_counter counter\nhello_world_counter %d\n" % counter
     return Response(result, mimetype='text/plain')
+
+app.run()
